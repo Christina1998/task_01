0d71a8cac121d83ec296b910c1db432c59598ccb
Kristina Shrestha <kristinashrestha2055@gmail.com>
2023-01-15 17:15:54 +0545

    Delete function in list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { createContext } from 'react';
 import './App.css';
 import Home from './components/Home';
-// import { Route, Switch, Routes } from 'react-router-dom';
-// import Sidebar from './components/Sidebar';
+
 
 const Data = createContext();
 
 function App() {
   return (
     <div className="App">
-     {/* <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/reports" element={<About />} /> */}
-      {/* <Route path="/lead-source" element={<LeadSourceMain />} /> */}
-
-    {/* </Routes> */}
     <Home/>
     </div>
   );
